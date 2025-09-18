58e9277f6b211510d0bd6d2719cc7196985b97ea
Naresh Perumalla <naresh.xplores777@gmail.com>
2025-09-18 15:03:55 +0530

    db and env changes

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,14 @@ const express=require("express");
 const dotenv=require("dotenv")
 dotenv.config();
 
+const DB=require("./src/config/db")
+
 const app=express();
 
 const port=process.env.PORT || 3000;
 app.use((req,res,next)=>{
     console.log("App is listening")
-    // next();
+    next();
 })
 
 app.get("/",(req,res)=>{
@@ -16,7 +18,27 @@ app.get("/",(req,res)=>{
     res.send("You can do it Myman")
 })
 
-app.listen(port,()=>{
-   
-    console.log(`The App is listening on ${port} port`)
+
+
+
+DB().then((res)=>{
+    console.log("Connected to DB")
+    
+}).catch((err)=>{
+    console.log("Something went Wrong")
 })
+
+
+app.listen(port,()=>{
+        console.log(`The App is listening on ${port} port`)
+    })
+
+
+    
+// app.listen(port,()=>{
+   
+//     console.log(`The App is listening on ${port} port`
+    
+//     )
+//     console.log(DB())
+// })
