d5fbd01103731694db26bab5c25c7b515bc17376
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-11-07 23:08:59 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 
-  // Import your Publishable Key fdgf dsfa dsa 
+  // Import your Publishable Key fdgf dsfa ds 
   const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
   if (!PUBLISHABLE_KEY) {
