79bd63f9bc4e4199f6910c83d154d4336f907964
naresh-perumalla-07 <naresh.xplores777@gmail.com>
2025-11-21 23:42:39 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(clerkMiddleware());
-app.use(express.json());
+app.use(express.json()); 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
